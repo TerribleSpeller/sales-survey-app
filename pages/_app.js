20a0cf3d7da9b4c4fa1ec43f31089de1d7ebede2
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2025-04-07 17:33:58 +0700

    Add custom styles for footer and layout adjustments in badhieracss.css
    - Introduced styles for the footer section with a background image.
    - Set width and margin properties for various row classes.
    - Defined text color and font styles for links and headings.
    - Ensured compatibility with existing layout by preserving certain styles.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+
 import "@/styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import "@/styles/badhieracss.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
